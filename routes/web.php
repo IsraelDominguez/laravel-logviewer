2a7958c5c89361d55ea4a10f5dcbfd14a8c2c473
idominguez <israel.dominguez.garcia@gmail.com>
2018-10-15 10:49:21 +0200

    Add Roles permissions

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,7 @@
 
 Route::group(['middleware' => ['web']], function () {
     Route::prefix('admin')->group(function () {
-        Route::group(['namespace' => 'Genetsis\LogViewer\Controllers'], function () {
+        Route::group(['middleware' => ['role:SuperAdmin'], 'namespace' => 'Genetsis\LogViewer\Controllers'], function () {
             Route::get('logviewer', 'LogViewerController@index')->name('log-viewer-home');
             Route::get('logviewer/{file}', 'LogViewerController@index')->name('log-viewer-file');
             Route::get('logviewer/{file}/delete', 'LogViewerController@delete')->name('log-viewer-delete');
