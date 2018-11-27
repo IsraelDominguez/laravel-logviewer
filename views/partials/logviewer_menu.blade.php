45c25e35a67094fca1a2d418245c4d9279a5ea58
idominguez <israel.dominguez.garcia@gmail.com>
2018-11-27 13:04:22 +0100

    log menu selected

--- views/partials/logviewer_menu.blade.php
+++ views/partials/logviewer_menu.blade.php
@@ -3,7 +3,7 @@
     <a href=""><i class="zmdi zmdi-format-list-bulleted zmdi-hc-fw"></i> Logs</a>
     <ul>
         @foreach(\Logviewer::getLogFiles() as $logFile)
-            <li class="{{ ($section=='campaign') ? 'navigation__active' : '' }}">
+            <li class="{{ ($section=='logs') ? 'navigation__active' : '' }}">
                 <a href="{{ route('log-viewer-file', ['file' => $logFile]) }}">{{ $logFile }}</a>
             </li>
         @endforeach
